233c13fa51499cd586c2b5239dfff839d031923b
Anthony Bobsin <anthony.bobsin@instacart.com>
2021-12-08 14:13:22 -0500

    stylin'

--- giftacart-frontend/src/App.js
+++ giftacart-frontend/src/App.js
@@ -1,13 +1,28 @@
 import * as React from "react";
-import { Admin, ListGuesser, Resource } from "react-admin";
+import { Admin, Resource } from "react-admin";
 import { UserCreate, UserEdit, UserList } from "./resources/users";
 import dataProvider from "./dataProvider";
 import { OrderList } from "./resources/orders";
+import { createTheme } from "@material-ui/core/styles";
+import green from "@material-ui/core/colors/green";
+import PersonIcon from '@material-ui/icons/Person';
+import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#388e3c",
+    },
+    secondary: {
+      main: "#d7ccc8",
+    }
+  }
+})
 
 const App = () => (
-  <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={UserList} edit={UserEdit} create={UserCreate} />
-    <Resource name="orders" list={OrderList} />
+  <Admin theme={theme} dataProvider={dataProvider}>
+    <Resource name="users" icon={PersonIcon} list={UserList} edit={UserEdit} create={UserCreate} />
+    <Resource name="orders" icon={LibraryBooksIcon} list={OrderList} />
   </Admin>
 );
 
